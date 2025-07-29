5a4bb5c272fad102ca8de10e9573aab3077fdd0b
Git GetReady <git@getready.io>
2025-07-29 16:49:54 +0000

    New package version for Telegram, version: 0.16.0-dev.38

--- Runtime/src/TelegramMessageReceiver.cs
+++ Runtime/src/TelegramMessageReceiver.cs
@@ -7,18 +7,26 @@ namespace RGN.Modules.Telegram
         public event System.Action OnFullscreenChanged;
         public event System.Action<string> OnFullscreenFailed;
 
+#if DEBUG
         private void Start()
         {
             Debug.Log("[TelegramMessageReceiver] Start");
         }
+#endif
 
         private void FullscreenChangedMessage()
         {
+#if DEBUG
+            Debug.Log("[TelegramMessageReceiver] Fullscreen changed");
+#endif
             OnFullscreenChanged?.Invoke();
         }
 
         private void FullscreenFailedMessage(string error)
         {
+#if DEBUG
+            Debug.Log("[TelegramMessageReceiver] Fullscreen failed, error: " + error);
+#endif
             OnFullscreenFailed?.Invoke(error);
         }
     }
